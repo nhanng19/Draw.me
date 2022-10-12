da1954902e57fdbc64c9f4bd4a75ad495235358e
Nodnarbrones <sr.bvillasenor@gmail.com>
2022-10-11 19:51:23 -0700

    relabled variables, routes, and directories

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const drawingRoutes = require('./drawingRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/drawing', drawingRoutes);
 
 module.exports = router;
