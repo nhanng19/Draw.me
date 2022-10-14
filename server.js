cc5afe89c37692c294d4c4b800760ea03b0f7980
nhan <nhanng19@gmail.com>
2022-10-14 03:17:28 -0700

    relable

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 300000,
+    maxAge: 3600000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
