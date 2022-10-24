b6f50bde4ad516555da7e948116ccba5fc7648ae
nhan <nhanng19@gmail.com>
2022-10-23 21:53:10 -0700

    mvc index

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const sketchRoutes = require('./sketchRoutes');
+const galleryRoutes = require('./galleryRoutes');
+const profileRoutes = require('./profileRoutes')
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/sketch', sketchRoutes);
+router.use('/gallery', galleryRoutes);
+router.use('/profile', profileRoutes);
+
 
 module.exports = router;
