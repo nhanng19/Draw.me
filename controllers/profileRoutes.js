e1ad36a37539e344dd697ee2dad98e8bd4b93f28
nhan <nhanng19@gmail.com>
2022-10-23 21:53:21 -0700

    mvc profile routes

---
+++ controllers/profileRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Drawing, User} = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Drawing }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
