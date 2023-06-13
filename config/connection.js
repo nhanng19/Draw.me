db53ec3f9e6521325e43d9450f41ec5ee7212213
Nhan Nguyen <nhanng19@gmail.com>
2023-06-13 13:27:16 -0700

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'containers-us-west-210.railway.app',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       port: 5995
     }
